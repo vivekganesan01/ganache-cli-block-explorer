58c55713d5a72cd7102cd84b426b7de4e62d04e1
Awasthi-Vinay <vinay.awasthi-ee@infineon.com>
2022-06-20 16:17:08 +0530

    Updated GUI and Functionality
    Updated GUI and functionality to display default ganache accounts on home page, txHash contained in each block and field to get transaction details using tx hash

---
+++ rpcRequestGo.go
@@ -0,0 +1,136 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+type EthRPC struct {
+	url    string
+	client httpClient
+	log    logger
+	Debug  bool
+}
+type logger interface {
+	Println(v ...interface{})
+}
+
+type httpClient interface {
+	Post(url string, contentType string, body io.Reader) (*http.Response, error)
+}
+
+type ethRequest struct {
+	ID      int           `json:"id"`
+	JSONRPC string        `json:"jsonrpc"`
+	Method  string        `json:"method"`
+	Params  []interface{} `json:"params"`
+}
+
+type ethResponse struct {
+	ID      int             `json:"id"`
+	JSONRPC string          `json:"jsonrpc"`
+	Result  json.RawMessage `json:"result"`
+	Error   *EthError       `json:"error"`
+}
+
+// EthError - ethereum error
+type EthError struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+}
+
+func (err EthError) Error() string {
+	return fmt.Sprintf("Error %d (%s)", err.Code, err.Message)
+}
+
+func (rpc *EthRPC) call(method string, target interface{}, params ...interface{}) error {
+	result, err := rpc.Call(method, params...)
+	if err != nil {
+		return err
+	}
+
+	if target == nil {
+		return nil
+	}
+
+	return json.Unmarshal(result, target)
+}
+
+// Call returns raw response of method call
+func (rpc *EthRPC) Call(method string, params ...interface{}) (json.RawMessage, error) {
+	request := ethRequest{
+		ID:      1,
+		JSONRPC: "2.0",
+		Method:  method,
+		Params:  params,
+	}
+
+	body, err := json.Marshal(request)
+	if err != nil {
+		return nil, err
+	}
+
+	response, err := rpc.client.Post(rpc.url, "application/json", bytes.NewBuffer(body))
+	if response != nil {
+		defer response.Body.Close()
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	if rpc.Debug {
+		rpc.log.Println(fmt.Sprintf("%s\nRequest: %s\nResponse: %s\n", method, body, data))
+	}
+
+	resp := new(ethResponse)
+	if err := json.Unmarshal(data, resp); err != nil {
+		return nil, err
+	}
+
+	if resp.Error != nil {
+		return nil, *resp.Error
+	}
+
+	return resp.Result, nil
+
+}
+
+// New create new rpc client with given url
+func newClient(url string, options ...func(rpc *EthRPC)) *EthRPC {
+	rpc := &EthRPC{
+		url:    url,
+		client: http.DefaultClient,
+		log:    log.New(os.Stderr, "", log.LstdFlags),
+	}
+	for _, option := range options {
+		option(rpc)
+	}
+
+	return rpc
+}
+
+/*
+func main() {
+	client := newClient("http://18.193.129.167:8545")
+
+	var accounts []string
+
+	_ = accounts
+	err := client.call("eth_accounts", &accounts)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(accounts)
+}*/
